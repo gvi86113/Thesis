8d8dd10bd779a38aad6401f6036dab51fada9b84
Yukai <33747259+gvi86113@users.noreply.github.com>
2021-03-09 15:48:00 +0800

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[1]:
 
 
 import numpy as np
@@ -38,20 +38,20 @@ df = pd.merge(T_I, B, how='left', on='ACCT_KEY')
 df
 
 
-# In[20]:
+# In[6]:
 
 
 describe = df.describe()[['B_FREQ','T_FREQ','P_BETA21','P_BETA65','P_BETA250','TXN_AMOUNT','PROFIT']]
 describe
 
 
-# In[21]:
+# In[7]:
 
 
 describe.to_csv('Describe.csv')
 
 
-# In[6]:
+# In[8]:
 
 
 # Histogram for the total profit of each user
@@ -60,27 +60,27 @@ plt.hist(x, bins=10)
 plt.gca().set(title='PROFIT Histogram', ylabel='PROFIT')
 
 
-# In[7]:
+# In[9]:
 
 
 # Histogram for the total profit of each user by different gender
 df['PROFIT'].hist(by=df['GENDER_CODE'])
 
 
-# In[8]:
+# In[10]:
 
 
 # Histogram for the total profit of each user by different job
 df['PROFIT'].hist(by=df['JOB_DESC'])
 
 
-# In[ ]:
+# In[11]:
 
 
 # Kernel Density Estimation for profit (by different Beta)
 
 
-# In[9]:
+# In[12]:
 
 
 # Histogram for the behavior frequence of each user
@@ -89,46 +89,78 @@ plt.hist(x, bins=10)
 plt.gca().set(title='Behavior frequence Histogram', ylabel='B_FREQ')
 
 
-# In[10]:
+# In[13]:
 
 
 # Histogram for the behavior frequence of each user by different gender
 df['B_FREQ'].hist(by=df['GENDER_CODE'])
 
 
-# In[11]:
+# In[14]:
 
 
 # Histogram for the behavior frequence of each user by different job
 df['B_FREQ'].hist(by=df['JOB_DESC'])
 
 
-# In[13]:
+# In[15]:
 
 
-from sklearn import linear_model
+# OLS regression for Profit to Behavior Frequency
 import statsmodels.api as sm
 from scipy import stats
 
+# transfer into a column-like array
 x = np.array(df['B_FREQ']).reshape(-1,1)
 y = df['PROFIT']
 
-X2 = sm.add_constant(x)
-est = sm.OLS(y, X2)
-est2 = est.fit()
-print(est2.summary())
+# add an intercept in the model
+X = sm.add_constant(x)
+model = sm.OLS(y, X)
+result = model.fit()
+print(result.summary())
 
 
-# In[14]:
+# In[16]:
 
 
 x = pd.concat([df['B_FREQ'],df['P_BETA250']],axis=1)
 y = df['PROFIT']
 
-X2 = sm.add_constant(x)
-model = sm.OLS(y, X2)
-model2 = model.fit()
-print(model2.summary())
+X = sm.add_constant(x)
+model = sm.OLS(y, X)
+result = model.fit()
+print(result.summary())
+
+
+# In[17]:
+
+
+X
+
+
+# In[33]:
+
+
+# set the dimension of the plot as 1x1x1
+ax = plt.subplot(111)
+plt.scatter(df['B_FREQ'], df['PROFIT'])
+ax.set_xlabel('B_FREQ')
+ax.set_ylabel('PROFIT')
+sm.qqline(ax, "r", x, y)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
